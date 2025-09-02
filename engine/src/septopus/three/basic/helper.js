35b0cf6422d301ab4cfccf50d4b1b6f138b75565
fuu <fu@yihaofang.com>
2025-09-03 07:46:51 +0800

    1. all three.js helper support;

--- engine/src/septopus/three/basic/helper.js
+++ engine/src/septopus/three/basic/helper.js
@@ -18,11 +18,14 @@ const helps={
     box:(cfg)=>{
         return new THREE.BoxHelper(cfg.object,cfg.color);
     },
+    plane:(cfg)=>{
+        return new THREE.PlaneHelper(cfg.object,cfg.color);
+    },
     arrow:(cfg)=>{
-
+        return new THREE.ArrowHelper(cfg.dir, cfg.origin, cfg.length, cfg.color, cfg.headLength, cfg.headWidth);
     },
     camera:(cfg)=>{
-
+        return new THREE.CameraHelper(cfg.camera,cfg.size,cfg.color);
     },
     grid:(cfg)=>{
         return new THREE.GridHelper(cfg.size,cfg.division,cfg.color[0],cfg.color[1]);
@@ -34,10 +37,10 @@ const helps={
         return new THREE.DirectionalLightHelper(cfg.light,cfg.size,cfg.color);
     },
     hemisphere:(cfg)=>{
-
+        return new THREE.HemisphereLightHelper(cfg.light,cfg.size,cfg.color);
     },
     spot:(cfg)=>{
-
+        return new THREE.SpotLightHelper(cfg.light,cfg.size,cfg.color);
     },
 }
 
